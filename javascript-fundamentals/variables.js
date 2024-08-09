f2f5de722bfecaa1407ff189cb734965a62ba03b
Johnny-Jackpot <sanya.nazarenko.up036@gmail.com>
2024-08-09 13:51:04 +0300

    variables

---
+++ javascript-fundamentals/variables.js
@@ -0,0 +1,16 @@
+"use strict";
+
+//https://javascript.info/variables#working-with-variables
+
+let admin;
+let name;
+name = "John";
+admin = name;
+
+console.log(admin);
+
+//https://javascript.info/variables#working-with-variables
+let outPlanetName = "Earth";
+let currentVisitor = "Alex";
+
+console.log(outPlanetName, currentVisitor);
