fc97409d813d6dde1d092607f9e66163c239a3dc
Johnny-Jackpot <sanya.nazarenko.up036@gmail.com>
2024-08-09 19:14:02 +0300

    basic operators

---
+++ src/javascript-fundamentals/basic-operators.js
@@ -0,0 +1,25 @@
+let x = 1;
+x = -x;
+// console.log(x);
+
+let a = 1;
+let b = 3;
+// console.log(3 - 1);
+
+// console.log(5 % 2);
+
+// console.log(2 ** 3);
+
+// console.log(9 ** (1 / 2));
+// console.log(27 ** (1 / 3));
+
+// console.log("my" + "string");
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log(2 + 2 + "1");
+// console.log("2" + 2 + 1);
+// console.log(6 - "2");
+
+// console.log(+true);
+// console.log(+false);
+// console.log(+"234");
