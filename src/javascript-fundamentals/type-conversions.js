7de251b04716bb36a55d9594f4000494f5c0c2fe
Johnny-Jackpot <sanya.nazarenko.up036@gmail.com>
2024-08-09 18:29:17 +0300

    type conversions

---
+++ src/javascript-fundamentals/type-conversions.js
@@ -0,0 +1,22 @@
+"use strict";
+
+let value = true;
+
+value = String(value);
+
+let x = "6" / "2";
+
+let str = "123";
+
+let num = Number(str);
+
+let str2 = "123.56";
+let num2 = Number(str2);
+
+let nan = Number("foo");
+
+console.log(Boolean(1));
+console.log(Boolean(0));
+console.log(Boolean("hello"));
+console.log(Boolean(""));
+console.log(Boolean("0"));
