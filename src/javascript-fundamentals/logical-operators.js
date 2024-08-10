e8ef1e0552d8adb03a81a334b36655ecd2b6105f
Johnny-Jackpot <sanya.nazarenko.up036@gmail.com>
2024-08-10 21:07:08 +0300

    logical operators

--- src/javascript-fundamentals/logical-operators.js
+++ src/javascript-fundamentals/logical-operators.js
@@ -22,3 +22,21 @@ console.log(between14And90(15));
 console.log(between14And90(89));
 console.log(between14And90(90));
 console.log(between14And90(91));
+
+let a = null;
+let b = 23;
+console.log(a ?? b);
+console.log(a !== null && a !== undefined ? a : b);
+
+let height = 0;
+console.log(height || 100);
+console.log(height ?? 100);
+
+let heigth = null;
+let width = null;
+let area = (heigth ?? 100) * (width ?? 500);
+console.log(area);
+
+//error
+//console.log(1 && 2 ?? 3);
+console.log((1 && 2) ?? 3);
