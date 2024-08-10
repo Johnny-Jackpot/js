4a7e355108ea2d9c7ff92740d4ec92e205aee12f
Johnny-Jackpot <sanya.nazarenko.up036@gmail.com>
2024-08-10 20:23:34 +0300

    comparisons

---
+++ src/javascript-fundamentals/comparisons.js
@@ -0,0 +1,34 @@
+console.log("Z" > "A");
+console.log("Glow" > "Glee");
+console.log("Bee" > "Be");
+console.log("2" > 1);
+console.log("1" == 1);
+
+console.log(Boolean(0));
+console.log(Boolean("0"));
+console.log("0" == 0);
+console.log(0 == false);
+console.log(0 === false);
+console.log("" == false);
+console.log(null === undefined);
+console.log(null == undefined);
+console.log(null > 0);
+console.log(null == 0);
+console.log(undefined == 0);
+console.log(null >= 0);
+console.log(undefined >= 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+console.log(undefined == 0);
+
+console.log(undefined == undefined);
+console.log(undefined === undefined);
+
+console.log(5 > 4);
+console.log("apple" > "pineapple");
+console.log("2" > "12");
+console.log(undefined == null);
+console.log(undefined === null);
+console.log(null == "\n0\n");
+console.log(null === +"\n0\n");
+console.log(null == +"\n0\n");
